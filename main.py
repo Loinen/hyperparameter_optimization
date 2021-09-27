cab00ecfd57ae2a8f77fb59d217eb7035c418ff6
Loinen <dance_cucumber@mail.ru>
2021-09-27 18:07:26 +0300

    add interpolation

--- main.py
+++ main.py
@@ -8,33 +8,68 @@ data = pd.read_excel("kaggle/well_log.xlsx", sheet_name=0)
 data.drop(columns=['SXO', 'Dtsyn', 'Vpsyn', 'sw new', 'sw new%', 'PHI2', 'ΔVp (m/s)'], inplace=True)
 # print(data)
 
+data[:100].plot(subplots=True, figsize=(15, 10))
+plt.xlabel("N")
+plt.legend(loc='best')
+plt.xticks(rotation='vertical')
+plt.show()
+
 # рисуем корреляционную матрицу
 # corr = data.corr()
 # sns.heatmap(corr, annot=True, fmt='.1f', cmap='Blues')
 # plt.show()
 
 # обходим массив и создаем недостающие значения глубины (остальные - NaN)
-counter = 0
 temp_df = pd.DataFrame()
 
 new_str = np.zeros(17)
 new_str[:] = np.nan
 
-for i in range(1, len(data.Depth)):
-    if data.Depth[i] > data.Depth[i - 1] + 0.15:
-        counter += 1
-        new_str[0] = np.round(data.Depth[i-1] + 0.15, 2)  # добавляем новое значение для глубины
+depth = data.Depth[0]
+depths_list = []
+while depth <= data.Depth.values[-1]:
+    depth = np.round(depth + 0.15, 2)
+    depths_list.append(depth)
+    if depth not in data.Depth.values:
+        new_str[0] = depth  # добавляем новое значение для глубины
         str_to_pandas = pd.DataFrame([new_str], columns=list(data.columns))
         temp_df = temp_df.append(str_to_pandas)
-        print(data.Depth[i], data.Depth[i - 1], str_to_pandas.Depth[0])
 
-print(counter, "новых значений добавлено")
-print(temp_df)
+print(len(temp_df), "новых значений добавлено")
 
 data = data.append(temp_df).sort_values(by=['Depth'])
+data.reset_index(inplace=True)
 print(data)
 
-data.to_csv('kaggle/well_log_add_depths.csv', index=False)
+data = data.interpolate(method='index')
+
+data[:100].plot(subplots=True, figsize=(15, 10))
+plt.xlabel("N")
+plt.legend(loc='best')
+plt.xticks(rotation='vertical')
+plt.show()
+
+# после интерполяции удаляем "лишние" старые значения, чтобы остался ряд с шагом .15
+
+inx_list = []
+depth = data.Depth[0]
+for i in range(1, len(data.Depth)):
+    if data.Depth[i] not in depths_list:
+        inx_list.append(i)
+
+data = data.drop(index=inx_list)
+data = data.drop(columns=["index"])
+
+data[:100].plot(subplots=True, figsize=(15, 10))
+plt.xlabel("N")
+plt.legend(loc='best')
+plt.xticks(rotation='vertical')
+plt.show()
+
+print(data)
+print(data[:25])
+
+data.to_csv('kaggle/well_log_interpolated.csv', index=False)
 
 
 
